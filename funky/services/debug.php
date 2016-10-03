d71f1e7dc553d8f76c8f0ddc38549bcff93a2ade
Matt Larson <mistermashu@gmail.com>
2016-10-02 21:00:03 -0500

    got rid of the error function

--- funky/services/debug.php
+++ funky/services/debug.php
@@ -7,21 +7,6 @@ class debug
 		var_dump($data);
 		echo '</pre>';
 	}
-	public function error($error='')
-	{
-		if(empty($error)) $error = 'An Error Occurred.';
-		
-		// print the error:
-		?>
-		<div style="border:1px solid red;background-color:rgba(255,0,0,0.3);">
-			<h2 style="color:red;">PHP Error</h2>
-			<p><?=$error?></p>
-		</div>
-		<?
-		
-		// error_log the error:
-		error_log($error);
-	}
 	public function exception($e)
 	{
 		echo 'error on website: '.$e->getMessage();
