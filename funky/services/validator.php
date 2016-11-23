624384eb9067d1eb032aff7a812833c13d75facc
Matt Larson <mistermashu@gmail.com>
2016-11-23 17:11:15 -0600

    simplified the default homepage

---
+++ funky/services/validator.php
@@ -0,0 +1,20 @@
+<?php
+// each function in this class will return TRUE if it's all good to go, and FALSE if it isn't.
+// if the function is passed a TRUE, that means that function will run
+class validator
+{
+	public function dbconfig($run=false)
+	{
+		if($run){
+			throw new \exception('the validator->dbconfig() cant set the db config because config is not writable. please set it in the files.');
+		}
+		$hasdbconfig = false;
+		try{
+			foreach(['db_server', 'db_user', 'db_password', 'db_name'] as $key){
+				f()->config->$key;
+			}
+			// in this context, the config service didn't complain at all, so we have all those config settings set.
+			$hasdbconfig = true;
+		}
+	}
+}
--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,1 @@
 <h2>Your Awesome Webthing</h2>
-<h3>Setup</h3>
