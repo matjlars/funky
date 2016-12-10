59ee2d30fce72dc86b95de8a49ff968de17a076a
Matt Larson <mistermashu@gmail.com>
2016-12-10 13:00:49 -0600

    added the time field

---
+++ funky/src/fields/time.php
@@ -0,0 +1,36 @@
+<?php
+namespace funky\fields;
+
+class time extends field
+{
+	private $format;
+	public function init($args)
+	{
+		// set the default format:
+		if(empty($args['format'])){
+			$this->format = 'h:i A';
+		}else{
+			$this->format = $args['format'];
+		}
+	}
+	public function set($val)
+	{
+		parent::set(strtotime($val));
+	}
+	public function get()
+	{
+		return $this->format($this->format);
+	}
+	public function format($format)
+	{
+		return \date($format, $this->val);
+	}
+	public function dbval()
+	{
+		return $this->val;
+	}
+	public function dbtype()
+	{
+		return 'time';
+	}
+}
