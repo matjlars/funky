fc06cfc442c4cbbb5a26c88964bb7e504ec37562
Matt Larson <mistermashu@gmail.com>
2016-12-11 15:11:47 -0600

    added the first() function to the modelquery

--- funky/src/core/modelquery.php
+++ funky/src/core/modelquery.php
@@ -112,6 +112,14 @@ class modelquery implements Iterator
 		return $sql;
 	}
 	
+	// returns the first model in the array of models.
+	// returns false if there are none.
+	public function first()
+	{
+		$this->run();
+		if(empty($this->models)) return false;
+		return array_shift($this->models);
+	}
 	public function count()
 	{
 		$this->run();
