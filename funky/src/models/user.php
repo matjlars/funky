e6341c69991ed90108a16bbb974cfd924912fe7f
Matt Larson <mistermashu@gmail.com>
2016-12-17 17:02:04 -0600

    started fixing db errors

--- funky/src/models/user.php
+++ funky/src/models/user.php
@@ -4,6 +4,10 @@ use core\model;
 
 class user extends model
 {
+	public function hasrole(string $role)
+	{
+		return $this->field('roles')->in($role);
+	}
 	public static function fields()
 	{
 		return f()->load->fields([
