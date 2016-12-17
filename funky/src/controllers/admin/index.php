3f3468275261a02b9fa617ccd11e443632b3e679
Matt Larson <mistermashu@gmail.com>
2016-12-17 14:51:28 -0600

    removed some artisan forge stuff

--- funky/src/controllers/admin/index.php
+++ funky/src/controllers/admin/index.php
@@ -5,18 +5,11 @@ class index
 {
 	public function __construct()
 	{
+		f()->access->enforce();
 		f()->template->view = 'admin';
 	}
 	public function index()
 	{
-		f()->access->enforce();
-		
-		// if I'm only an artisan, the only thing I can do is edit myself, so just go there if there is an artisan
-		if(f()->access->user()->field('roles')->only('artisan')){
-			$artisan_id = f()->db->query('select id from artisans where user_id = '.f()->access->user_id())->val('id');
-			if(!empty($artisan_id)) f()->path->redirect('admin/artisans/edit/'.$artisan_id);
-		}
-		
 		f()->load->view('admin/index/index');
 	}
 }
