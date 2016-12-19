faa6db19b03b476cc3c81cdd09c61e7e3fb9224d
Matt Larson <mistermashu@gmail.com>
2016-12-18 19:55:16 -0600

    added support for different int sizes in the integer field

--- funky/src/fields/integer.php
+++ funky/src/fields/integer.php
@@ -5,9 +5,11 @@ class integer extends field
 {
 	protected $length = 11;
 	protected $signed = true;
+	protected $bytes = 4;
 	
 	public function init($args)
 	{
+		if(isset($args['signed'])) $this->signed = $args['signed'];
 		if(!empty($args['length'])) $this->length = $args['length'];
 		if(!empty($args['min'])){
 			$this->validators[] = function($val){
@@ -19,16 +21,32 @@ class integer extends field
 				if($val < $args['max']) return 'more than '.$args['max'];
 			};
 		}
-		if(isset($args['signed'])){
-			$this->signed = $args['signed'];
+		
+		if(!empty($args['bytes'])){
+			$this->bytes = $args['bytes'];
+			$validbytes = array_keys(static::bytetypes());
+			if(!in_array($this->bytes, $validbytes)){
+				throw new \exception('a "bytes" value of "'.$this->bytes.'" is invalid. it must be one of ['.implode(', ', $validbytes).']');
+			}
 		}
 	}
 	public function dbtype()
 	{
-		$sql = 'int('.$this->length.')';
+		$bytetypes = $this->bytetypes();
+		$sql = $bytetypes[$this->bytes].'('.$this->length.')';
 		if(!$this->signed){
 			$sql .= ' unsigned';
 		}
 		return $sql;
+	}
+	protected static function bytetypes()
+	{
+		return array(
+			1=>'tinyint',
+			2=>'smallint',
+			3=>'mediumint',
+			4=>'int',
+			8=>'bigint',
+		);
 	}
 }
