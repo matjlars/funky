ac8a252a4757b423d9bf925e2a7db85b630705a0
Matt Larson <mistermashu@gmail.com>
2017-02-05 12:09:14 -0600

    made the lorem ipsum generator service

--- funky/src/services/env.php
+++ funky/src/services/env.php
@@ -4,11 +4,16 @@ namespace funky\services;
 class env
 {
 	// returns true or false depending on if this is running on a local server
-	// so basically, a TRUE means it's being developed, and a FALSE means it's on a hosted environment
-	// keep in mind, this can be spoofed, so don't allow permissions due to this
+	// feel free to overwrite this function with your own conditions
 	public function islocal()
 	{
+		// if the server name is "localhost", we're certainly on local
 		if($_SERVER['SERVER_NAME'] == 'localhost') return true;
+		
+		// if the server name ends in ".local", we're also certainly on local
+		if(substr($_SERVER['SERVER_NAME'], -6) == '.local') return true;
+		
+		// otherwise, we may not be, so default to no.
 		return false;
 	}
 }
--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,3 @@
 <h2>Homepage</h2>
+
+<?=f()->lorem->get(500)?>
