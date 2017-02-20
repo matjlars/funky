1bec6c6a9843f0f05e3925894f0edb35cc2170cb
Matt Larson <mistermashu@gmail.com>
2017-02-19 19:48:41 -0600

    fixed date field view

--- funky/views/fields/date/view.php
+++ funky/views/fields/date/view.php
@@ -1,2 +1,2 @@
 <label for="<?=$field->name()?>"><?=$field->label()?></label>
-<input type="date" name="<?=$field->name()?>" id="<?=$field->name()?>" value="<?=$field->get()?>"/>
+<input type="date" name="<?=$field->name()?>" id="<?=$field->name()?>" value="<?=$field->format('Y-m-d')?>"/>
