cb6a6d112ffc4a98873a39a67711dd29ba020413
Matt Larson <mistermashu@gmail.com>
2017-02-19 21:10:16 -0600

    made the enum field

---
+++ funky/src/fields/enum.php
@@ -0,0 +1,27 @@
+<?php
+namespace funky\fields;
+
+class enum extends field
+{
+	private $values = array();
+	
+	public function init($args)
+	{
+		if(empty($args['values'])) throw new \exception('enum field '.$this->name.' requires a "values" arg. this should contain strings that are the enum keys in the database');
+		if(!is_array($args['values'])) throw new \exception('enum field '.$this->name.' has a "values" arg, but it is not an array. It should be an array of strings for the enum keys');
+		$this->values = $args['values'];
+		
+		// validate on 
+		$this->validators[] = function($val){
+			if(!in_array($val, $this->values)) return 'contains an invalid value ('.$val.'). It should be one of ['.implode(',',$this->values).']';
+		};
+	}
+	public function values()
+	{
+		return $this->values;
+	}
+	public function dbtype()
+	{
+		return 'enum(\''.implode('\',\'',$this->values).'\')';
+	}
+}
