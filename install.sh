fc42879fa3c9aaae3e777029eddda2f576de9b7f
mistermashu <mistermashu@gmail.com>
2017-07-01 11:57:45 -0500

    removed some debug output

--- install.sh
+++ install.sh
@@ -5,8 +5,5 @@ MISTERMASHU=$(dirname $FUNKY)
 VENDOR=$(dirname $MISTERMASHU)
 YOURPROJECT=$(dirname $VENDOR)
 
-echo $FUNKY
-echo $YOURPROJECT
-
 cp -rf $FUNKY/scaffold/* $YOURPROJECT
 mv _gitignore .gitignore
