183b14ac1c78f5b03559286008afea4c9ead3dbc
Matt Larson <mistermashu@gmail.com>
2018-11-16 22:26:08 -0600

    added markdown helper link to the default markdown field view

--- views/fields/markdown/view.php
+++ views/fields/markdown/view.php
@@ -1,2 +1,3 @@
 <label for="<?=$field->name()?>"><?=$field->label()?></label>
 <textarea name="<?=$field->name()?>" id="<?=$field->name()?>" rows="10" cols="50"><?=$field->get()?></textarea>
+<em>This content area uses <a href="#" onclick="markdown.help();return false;">Markdown</a>.</em>
