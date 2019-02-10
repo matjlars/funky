d11bc22e973f1fbcbe1a5fe1b77be7d9eb728a2a
Matt Larson <mistermashu@gmail.com>
2019-02-10 11:09:05 -0600

    made a State field view

---
+++ views/fields/state/view.php
@@ -0,0 +1,7 @@
+<label for="<?=$field->name()?>"><?=$field->label()?></label>
+<select id="<?=$field->name()?>" name="<?=$field->name()?>">
+	<option value="">&mdash; Choose One &mdash;</option>
+	<?foreach(\funky\fields\base\state::ALL as $key=>$name){?>
+		<option value="<?=$key?>"<?=($key==$field->dbval())?' selected':''?>><?=$name?></option>
+	<?}?>
+</select>
