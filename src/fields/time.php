a7535c671c6794c962ebaac897f6182fbd7e517f
Matt Larson <mistermashu@gmail.com>
2020-08-04 17:06:11 -0500

    fixed the time field

--- src/fields/time.php
+++ src/fields/time.php
@@ -16,21 +16,25 @@ class time extends \funky\fields\field
 
 		if(isset($args['default']) && $args['default'] == 'now') $this->val = time();
 	}
+
 	public function set($val)
 	{
 		parent::set(strtotime($val));
 	}
+
 	public function get()
 	{
 		return $this->format($this->format);
 	}
+
 	public function format($format)
 	{
 		return \date($format, $this->val);
 	}
+
 	public function dbval()
 	{
-		return $this->val;
+		return $this->format('H:i:s');
 	}
 
 	public function dbtype()
