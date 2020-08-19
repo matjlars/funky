551079e21cbb92bc461db3b5fbb3ea2e7ee86b10
Matt Larson <mistermashu@gmail.com>
2020-08-19 15:46:39 -0500

    automated admin nav. also its extendable now.

--- views/templates/admin.php
+++ views/templates/admin.php
@@ -8,7 +8,19 @@
 </head>
 <body>
 	<h1>Admin</h1>
-	<?=f()->view->load('admin/nav')?>
+	<nav><?php
+		if(f()->access->isloggedin()){
+			foreach(f()->admin->nav_links() as $path=>$name){
+				?><a href="<?=$path?>"<?=(f()->url->iscurrent($path))?' class="active"':''?>><?=$name?></a><?php
+			}
+			?><aside>
+				<?php if(f()->access->hasrole('dev')){?>
+					<a href="/admin/admin">dev</a>
+				<?php }?>
+				<a href="/admin/logout">Logout</a>
+			</aside>
+		<?php }?>
+	</nav>
 	<?php if(isset($premainview)){?>
 		<?=f()->view->load($premainview)?>
 	<?php }?>
