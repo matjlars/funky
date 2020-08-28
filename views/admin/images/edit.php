39ca3ed53691dd0f2daf89cae2658a868590d563
Matt Larson <mistermashu@gmail.com>
2020-08-28 13:53:01 -0500

    image markdown snippet

--- views/admin/images/edit.php
+++ views/admin/images/edit.php
@@ -19,6 +19,14 @@
 		<em>Describe the image in detail. <a href="https://moz.com/learn/seo/alt-text" target="_blank">How to write good alt text</a></em>
 	</div>
 
+	<?php if($image->exists()){?>
+		<div class="field">
+			<label for="markdown_snippet">Markdown Snippet</label>
+			<input type="text" id="markdown_snippet" value="[img.<?=$image->id?>]" readonly>
+			<em>Copy this in to any markdown field to add this image.</em>
+		</div>
+	<?php }?>
+
 	<a class="button" href="/admin/images">Back</a>
 	<input type="submit" value="Save" class="green button"/>
 </form>
