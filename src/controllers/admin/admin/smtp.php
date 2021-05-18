263ff3e25ed1eaeff3ec3ae7ab9c2decb32f24ea
Matt Larson <matjlars@gmail.com>
2021-05-18 18:56:57 -0500

    smtp editor for devs

---
+++ src/controllers/admin/admin/smtp.php
@@ -0,0 +1,24 @@
+<?php
+namespace funky\controllers\admin\admin;
+
+class smtp
+{
+	public function __construct()
+	{
+		f()->access->enforce('dev');
+		f()->template->view = 'admin';
+		f()->template->premainview = 'admin/admin/subnav';
+	}
+
+	public function index()
+	{
+		if(!empty($_POST)){
+			foreach($_POST as $key=>$val){
+				f()->config->$key = $val;
+			}
+			f()->flash->success('Saved SMTP config!');
+		}
+
+		return f()->view->load('admin/admin/smtp/index');
+	}
+}
--- views/admin/admin/subnav.php
+++ views/admin/admin/subnav.php
@@ -1,8 +1,14 @@
+<?php
+$links = [
+	'admin/admin/users'=>'Users',
+	'admin/admin/config'=>'Config',
+	'admin/admin/smtp'=>'SMTP',
+	'admin/admin/database'=>'Database',
+];
+?>
+
 <nav>
-	<a href="<?=f()->url->get('admin/admin/validator')?>"<?=(f()->url->iscurrent('admin/admin/validator'))?' class="active"':''?>>Site Validator</a>
-	<a href="<?=f()->url->get('admin/admin/users')?>"<?=(f()->url->iscurrent('admin/admin/users'))?' class="active"':''?>>Users</a>
-	<a href="<?=f()->url->get('admin/admin/logs')?>"<?=(f()->url->iscurrent('admin/admin/logs'))?' class="active"':''?>>Logs</a>
-	<a href="<?=f()->url->get('admin/admin/config')?>"<?=(f()->url->iscurrent('admin/admin/config'))?' class="active"':''?>>Config</a>
-	<a href="<?=f()->url->get('admin/admin/sitemap')?>"<?=(f()->url->iscurrent('admin/admin/sitemap'))?' class="active"':''?>>Sitemap</a>
-	<a href="<?=f()->url->get('admin/admin/database')?>"<?=(f()->url->iscurrent('admin/admin/database'))?' class="active"':''?>>Database</a>
+<?php foreach($links as $path => $label){?>
+	<a href="<?=f()->url->get($path)?>"<?=(f()->url->iscurrent($path)) ? ' class="active"':''?>><?=$label?></a>
+<?php }?>
 </nav>
