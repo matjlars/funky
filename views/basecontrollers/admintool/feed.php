43e3bd31be431fd50b6a9a19d44d19dd5fecfb3e
Matt Larson <matjlars@gmail.com>
2022-05-27 14:57:10 -0500

    add sort icon to auto admintool view for models with sort_id

--- views/basecontrollers/admintool/feed.php
+++ views/basecontrollers/admintool/feed.php
@@ -4,6 +4,9 @@
 			<a href="/<?=$url_path?>/edit/<?=$o->id?>">
 				<?=$o->bridge_label()?>
 			</a>
+			<?php if($o->has_field('sort_id')){?>
+				<a class="sort icon"></a>
+			<?php }?>
 			<a class="delete icon" title="Delete this <?=$modelname?>" onclick="feedpage.delete('<?=$o->id?>');return false;" href="#"></a>
 		</li>
 	<?php }?>
