e325e214fd2c6f26c835b98a0e7c66f3e5a81f9f
Matt Larson <matjlars@gmail.com>
2022-08-22 21:47:08 -0500

    fixed state field view

--- views/fields/state/view.php
+++ views/fields/state/view.php
@@ -1,7 +1,7 @@
 <label for="<?=$field->name()?>"><?=$field->label()?></label>
 <select id="<?=$field->name()?>" name="<?=$field->name()?>">
 	<option value="">&mdash; Choose One &mdash;</option>
-	<?php foreach(\funky\fields\base\state::ALL as $key=>$name){?>
+	<?php foreach(\funky\fields\state::ALL as $key=>$name){?>
 		<option value="<?=$key?>"<?=($key==$field->dbval())?' selected':''?>><?=$name?></option>
 	<?php }?>
 </select>
