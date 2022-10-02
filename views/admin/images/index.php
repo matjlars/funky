136295e8ffa8cbf36cd9fcbd9ceff0af88a0ef84
Matt Larson <matjlars@gmail.com>
2022-10-02 16:10:09 -0500

    tweaking admin html

--- views/admin/images/index.php
+++ views/admin/images/index.php
@@ -1,10 +1,8 @@
-<section>
-	<header>
-		<h2>Images</h2>
-		<a class="new button" href="/admin/images/edit/0">New Image</a>
-	</header>
-	<div id="results"></div>
-</section>
+<header>
+	<h2>Images</h2>
+	<a class="new button" href="/admin/images/edit/0">New Image</a>
+</header>
+<div id="results"></div>
 
 <script>
 feedpage.init('/admin/images', 'Image');
--- views/basecontrollers/admintool/index.php
+++ views/basecontrollers/admintool/index.php
@@ -1,10 +1,8 @@
-<section>
-	<header>
-		<h2><?=ucwords($modelname_plural)?></h2>
-		<a class="new button" href="<?=f()->url->get($url_path.'/edit/0')?>">New <?=$modelname?></a>
-	</header>
-	<div id="results"></div>
-</section>
+<header>
+	<h2><?=ucwords($modelname_plural)?></h2>
+	<a class="new button" href="<?=f()->url->get($url_path.'/edit/0')?>">New <?=$modelname?></a>
+</header>
+<div id="results"></div>
 
 <script>
 feedpage.init('/<?=$url_path?>', '<?=$modelname?>');
